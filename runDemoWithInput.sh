7e3fd62bded6b14973a5b1b6e6bbaa6e70bd7bde
Kevin Nowak <nowakk96@icloud.com>
2022-03-28 18:25:43 +0200

    Run script updated

--- runDemoWithInput.sh
+++ runDemoWithInput.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
+if [ "$#" -ne 6 ]
+then
+  echo "Invalid arguments provided! To run this script you have to provide six arguments:"
+  echo "1. Board dimension"
+  echo "2. Winning condition"
+  echo "3. Starting player (X or O)"
+  echo "4. Winning player (X or O)"
+  echo "5. Winning type (e.g. horizontal)"
+  echo "6. Winning line"
+  
+  exit 1
+fi
+
+
 mvn clean package
-java -jar target/*.jar $0 $1 $2 $3 $4
+java -jar target/*.jar $1 $2 $3 $4 $5 $6
--- src/main/java/com/github/budison/demo/Demo.java
+++ src/main/java/com/github/budison/demo/Demo.java
@@ -9,10 +9,10 @@ public class Demo {
     public Demo() {
     }
     public Demo(ArgsReader argsReader) {
-
+        System.out.println("Demo Mode with arguments");
     }
 
     public void run() {
-        System.out.println("Demo Mode");
+        System.out.println("Running Demo Mode");
     }
 }
