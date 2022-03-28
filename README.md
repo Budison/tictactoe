aad1710e912f0dab70e7ba94440830440cd85bc8
Kevin Nowak <nowakk96@icloud.com>
2022-03-28 09:54:58 +0200

    README.adoc updated

--- README.md
+++
@@ -1,3 +0,0 @@
-# tictactoe2
-
-This project was created on 2022-03-28 from a template.
--- src/main/java/com/github/budison/Main.java
+++ src/main/java/com/github/budison/Main.java
@@ -1,5 +1,8 @@
 package com.github.budison;
 
+/**
+ * @author Kevin Nowak
+ */
 public class Main {
 
     public static void main(String[] args) {
